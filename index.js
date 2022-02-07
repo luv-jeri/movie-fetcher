d4c45bcae5c3b7bcbf8613e44dcd04caa5ec12a6
luv-jeri <luvvjeri@gmail.com>
2022-02-07 19:19:09 +0530

    Added : Basic Func. of login.

---
+++ index.js
@@ -0,0 +1,31 @@
+const login = () => {
+  const email =
+    document.getElementById(
+      'email'
+    ).value; // Grab the email from the email input field and stored it in a variable
+
+  const password =
+    document.getElementById(
+      'password'
+    ).value; // Grab the password from the email input field and stored it in a variable
+
+  // ! ERROR :  Check if the email and password are not empty
+  if (email === '' || password === '') {
+    alert(
+      'Please provide  your email and password'
+    );
+    return;
+  }
+
+  if (
+    email === 'sanjay@gmail.com' &&
+    password === '1234'
+  ) {
+    alert('Login Successful');
+  }
+};
+
+// Login button
+document
+  .getElementById('login')
+  .addEventListener('click', login); // Add an event listener to the login button
