85dc7569f6ab911aa87755061c12786f4b3a11c4
luv-jeri <luvvjeri@gmail.com>
2022-02-08 19:58:46 +0530

    Added fetch online

--- javascript/home.js
+++ javascript/home.js
@@ -1,6 +1,32 @@
-const logged = localStorage.getItem('isLoggedIn');
+const logged = localStorage.getItem(
+  'isLoggedIn'
+);
 
-if(!logged){
-    location.pathname = 'index.html';
+if (!logged) {
+  location.pathname = 'index.html';
 }
 
+fetch(
+  'https://reqres.in/api/users?page=2'
+)
+  .then((res) => {
+    res.json().then((data) => {
+      data.data.forEach((element) => {
+        const div =
+          document.createElement('div');
+
+        div.innerHTML = `
+            <div>${element.first_name} ${element.last_name}</div>
+            `;
+        document
+          .getElementById('list')
+          .appendChild(div);
+      });
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log('finally');
+  });
