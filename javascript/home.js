019bb98c3477e1eef2af2ef97d92fa8d9c7fcddc
Hrushi01 <hrushikesh.ambike@gmail.com>
2022-02-14 05:32:57 +0530

    Home pg -- Card Styling Updated

--- javascript/home.js
+++ javascript/home.js
@@ -41,30 +41,30 @@
 
 //  'https://jsonplaceholder.typicode.com/users'
 
-const getData = async () => {
-  const { data } = await axios(
-    'https://reqres.in/api/users?page=2'
-  );
+const getData = async() => {
+    const { data } = await axios(
+        'https://reqres.in/api/users?page=2'
+    );
 
-  console.log(data.data);
+    console.log(data.data);
 
-  data.data.forEach((element) => {
-    const card =
-      document.createElement('div');
-    //# <div></div>
-    card.classList.add('card'); //  Styling
-    // # <div class="card"></div>
-    card.innerHTML = `
-    <div>${element.first_name} ${element.last_name}</div>
-    <img class="img" src="${element.avatar}" />
+    data.data.forEach((element) => {
+        const card =
+            document.createElement('div');
+        //# <div></div>
+        card.classList.add('card'); //  Styling
+        // # <div class="card"></div>
+        card.innerHTML = `<img class="img" src="${element.avatar}" /> 
+    ${element.first_name} ${element.last_name}
+    
     `;
-    //# <div  class="card">${element.name}</div>
-    const container =
-      document.getElementById(
-        'card-container'
-      );
-    container.appendChild(card);  
-  });
+        //# <div  class="card">${element.name}</div>
+        const container =
+            document.getElementById(
+                'card-container'
+            );
+        container.appendChild(card);
+    });
 };
 
 getData();
